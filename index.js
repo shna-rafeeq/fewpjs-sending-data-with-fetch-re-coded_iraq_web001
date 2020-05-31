a55da8d26e2b5d485befb8f59c6fdd7259fa7b30
shna rafeeq <shnar.majeed@gmail.com>
2020-05-31 19:45:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(){
+  
+  let userData = {
+    name: "Steve",
+    email:"steve@steve.com"
+  };
+  
+  let configFunc = {
+   method: 'POST',
+   headers:{
+    'Content-Type':'application/json',
+    'Accept':'application/json'
+   },
+    body: JSON.stringify(userData)
+  };
+  
+ return fetch("http://localhost:3000/users", configFunc)
+ .then(function(response){
+   return response.json();
+ })
+ 
+ .then(function(json){
+   console.log(json);
+   
+ })
+ 
+}
+submitData();
