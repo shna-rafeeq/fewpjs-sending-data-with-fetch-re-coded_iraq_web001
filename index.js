842702ea499255740c670715601b7f2aa95da9db
shna rafeeq <shnar.majeed@gmail.com>
2020-05-31 20:11:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 // Add your code here
+
 function submitData(){
   
-  let userData = {
-    name: "Steve",
-    email:"steve@steve.com"
-  };
-  
-  let configFunc = {
-   method: 'POST',
-   headers:{
-    'Content-Type':'application/json',
-    'Accept':'application/json'
-   },
-    body: JSON.stringify(userData)
-  };
-  
- return fetch("http://localhost:3000/users", configFunc)
- .then(function(response){
-   return response.json();
- })
- 
- .then(function(json){
-  consle.lo
+    let userData = {
+      name: "Steve",
+      email:"steve@steve.com"
+    };
+    
+    let configFunc = {
+     method: 'POST',
+     headers:{
+      'Content-Type':'application/json',
+      'Accept':'application/json'
+     },
+      body: JSON.stringify(userData)
+    };
+    
+   return fetch("http://localhost:3004/users", configFunc)
+   .then(function(response){
+     return response.json();
+   })
    
- })
- 
-}
-submitData();
+   .then(function(json){
+   let a = document.getElementsByTagName('body')[0];
+   let p = document.createElement('p');
+   p.innerHTML = json.id;
+   a.appendChild(p);
+   })
+   .catch(function(error){
+    let b = document.getElementsByTagName('body')[0];
+    let q = document.createElement('p');
+    q.innerHTML = error.message;
+    b.appendChild(q);
+   })
+   
+  }
+  submitData();
